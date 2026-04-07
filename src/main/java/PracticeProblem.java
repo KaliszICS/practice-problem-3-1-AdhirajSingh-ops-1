2787fa99ebeb8e8157eb6b07e934e4f3177a68d6
AdhirajSingh-ops <adhiraj.singh3@student.tdsb.on.ca>
2026-04-07 13:07:40 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,23 @@ public class PracticeProblem {
 
 	}
 
-	
-
+	public static int sumOfDigits(int x) {
+		if (x < 0) {
+			return sumOfDigits(-x);
+		}
+		if (x < 10){
+			return x;
+		}
+		return (x % 10) + sumOfDigits(x/10);
+	}
+	public static int factorial(int n) {
+		if (n < 0) {
+			return 0;
+		}
+		if (n == 0) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
 }
+
